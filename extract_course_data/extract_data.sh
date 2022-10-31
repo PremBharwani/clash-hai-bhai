4efc469e157acb9c486265a6e45a49fa2758473d
PremBharwani <premr20@iitk.ac.in>
2022-11-01 05:03:05 +0530

    Add bash script to extract data and move it to the web app dir

---
+++ extract_course_data/extract_data.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Executing the python script first.
+python3 scrape_csv.py
+
+# Moving the newly created courses.json into the frontend/js directory
+mv courses.json ../frontend/js/
+
+# Changing the cwd to the frontend/js directory
+cd ../frontend/js/
+
+# Renaming the courses.json file to course_data.js
+mv courses.json course_data.js
+
+# Adding the "export const course_data =" to the beginning of the file
+sed -i '1s/^/export const course_data = /' course_data.js
+
