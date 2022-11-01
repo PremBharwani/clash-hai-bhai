4db0ced99555499ae30c618fda6b80c61c7ad746
Rahul Jha <theslytherin@yandex.com>
2022-11-01 14:13:48 +0530

    fixed the clash code

--- frontend/js/get_course_utils.js
+++ frontend/js/get_course_utils.js
@@ -92,7 +92,7 @@ Output: [['1000', '1100'], ['5800', '5900'], ['10600', '10700'], ['3400', '3500'
 */
 function merge_course_times(courses){
     var time_intervals = []
-    console.log(courses)
+    // console.log(courses)
     for (let i = 0; i < courses.length; i++){
         // console.log(template[i])
         var course_deets = get_course_timings(courses[i])
@@ -122,14 +122,14 @@ function return_bad_intervals(current_timings, course){
     var bad_intervals = []
     for (let i = 0; i < course_times.length; i++){
         for (let j = 0; j < current_timings.length; j++){
-            if(course_times[i][0] >= current_timings[j][0]){
-                if (course_times[i][0] <= current_timings[j][1]){
+            if(parseInt(course_times[i][0]) >= parseInt(current_timings[j][0])){
+                if (parseInt(course_times[i][0]) <= parseInt(current_timings[j][1])){
                     bad_intervals.push([course_times[i], current_timings[j]])
                     return bad_intervals
                 }
             }
-            else if(course_times[i][1] >= current_timings[j][0]){
-            if (course_times[i][1] <= current_timings[j][1]){
+            else if(parseInt(course_times[i][1]) >= parseInt(current_timings[j][0])){
+            if (parseInt(course_times[i][1]) <= parseInt(current_timings[j][1])){
                 bad_intervals.push([course_times[i], current_timings[j]])
                 return bad_intervals
             }
@@ -172,19 +172,12 @@ Example:
 get_eligible_courses(['EE210A', 'TA202A'], "CGS")
 Output: 
 0: Object { n_course: "110", dept: "CGS", course_name: "APPLIED COGNITIVE SCIENCE(CGS402A)", … }
-​
-1: Object { n_course: "111", dept: "CGS", course_name: "EYE-TRACKING AND VR AS TOOLS IN COGNITIVE SCIENCE(CGS612A)", … }
-​
-2: Object { n_course: "112", dept: "CGS", course_name: "INTRODUCTION TO PSYCHOLINGUISTICS(CGS621A)", … }
-​
-3: Object { n_course: "113", dept: "CGS", course_name: "TOPICS IN TRANSLATIONAL NEUROSCIENCE(CGS641A)", … }
-​
-4: Object { n_course: "115", dept: "CGS", course_name: "NEUROBIOLOGY OF AFFECT & MOTIVATION(CGS698D)", … }
-​
-5: Object { n_course: "116", dept: "CGS", course_name: "TOPICS IN VISUAL PERCEPTION(CGS698E)", … }
-
+​1: Object { n_course: "111", dept: "CGS", course_name: "EYE-TRACKING AND VR AS TOOLS IN COGNITIVE SCIENCE(CGS612A)", … }
+​2: Object { n_course: "112", dept: "CGS", course_name: "INTRODUCTION TO PSYCHOLINGUISTICS(CGS621A)", … }
+​3: Object { n_course: "113", dept: "CGS", course_name: "TOPICS IN TRANSLATIONAL NEUROSCIENCE(CGS641A)", … }
+​4: Object { n_course: "115", dept: "CGS", course_name: "NEUROBIOLOGY OF AFFECT & MOTIVATION(CGS698D)", … }
+​5: Object { n_course: "116", dept: "CGS", course_name: "TOPICS IN VISUAL PERCEPTION(CGS698E)", … }
 6: Object { n_course: "117", dept: "CGS", course_name: "PHD THESIS(CGS799)", … }
-
 7: Object { n_course: "118", dept: "CGS", course_name: "MS - RESEARCH THESIS(CGS899)", … }
 */
 export function get_eligible_courses(current_courses, target_dept){
