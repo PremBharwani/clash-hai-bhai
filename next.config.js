2ef2a99429854da22f06aa4bc5efea299ac922eb
Abhishek Shree <abhi1234456@gmail.com>
2022-11-05 00:52:01 +0530

    Module update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: './'
+}
